1c423b7d2bf813955ad970ac66edf30d2cc624db
Ryan Haas <Ryan Haas>
2017-02-27 17:45:51 -0600

    Completed addTodo

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -3,37 +3,48 @@ const TodoList = require('TodoList');
 const AddTodo = require('AddTodo');
 const TodoSearch = require('TodoSearch');
 
+const uuid = require('node-uuid');
+
 
 var TodoApp = React.createClass({
 
-
+	//uuid is a node package that creates unique id's based on the time.
 	getInitialState: function() {
 		return {
 			showCompleted: false,
 			searchText: '',
 			todos: [
 				{
-					id: 1,
+					id: uuid(),
 					text: 'Walk the dog'
 				},
 				{
-					id: 2,
+					id: uuid(),
 					text: 'Clean the yard'
 				},
 				{
-					id: 3,
+					id: uuid(),
 					text: 'Mow the lawn'
 				},
 				{
-					id: 4,
+					id: uuid(),
 					text: 'Jump over the ball'
 				}
 			]
 		};
 	},
 
+	//this adds a todo item to the oriignal todos array using the spread operator.
 	handleAddTodo: function(text) {
-		alert('New todo: ' + text);	
+		this.setState({
+			todos: [
+				...this.state.todos, 
+				{
+					id: uuid(),
+					text: text
+				}
+			]
+		})	
 	},
 	handleSearch: function(showCompleted, searchText) {
 		this.setState({
