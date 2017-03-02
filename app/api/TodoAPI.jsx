84ceea2d48994cf591c5add68ac34f2c22564e4c
Ryan Haas <Ryan Haas>
2017-03-01 18:09:04 -0600

    Add search functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -20,5 +20,41 @@ module.exports = {
 		}
 
 		return $.isArray(todos) ? todos : [];
-	}
+	},
+
+	//used to filter out todos
+	filterTodos: function(todos, showCompleted, searchText) {
+		var filteredTodos = todos;
+
+		//filter by showCompleted
+		var filteredTodos = filteredTodos.filter( (todo) => {
+			return !todo.completed || showCompleted;
+		});
+		
+		//filter by serachtext
+		filteredTodos = filteredTodos.filter((todo) => {
+			if(searchText === '') {
+				return true;
+			} else if ( todo.text.toLowerCase().indexOf(searchText) > -1 ) {
+				return true;
+			} else {
+				return false;
+			}
+		});
+
+		//sort todos with non-completed first
+		filteredTodos.sort((a, b) => {
+			if(!a.completed && b.completed) {
+				return -1;
+			} else if (a.completed && !b.completed) {
+				return 1;
+			} else {
+				return 0;
+			}
+		});
+
+		return filteredTodos;
+	},
+
+
 };
